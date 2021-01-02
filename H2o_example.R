e08e902b8729fc4a9f7b26d902858cd5f0e89788
Homer-Sleep <ryand1@goldmail.etsu.edu>
2021-01-02 11:45:16 -0500

    initail Docs

---
+++ H2o_example.R
@@ -0,0 +1,24 @@
+library(h2o)
+h2o.init()
+
+# Import a sample binary outcome train/test set into H2O
+train <- h2o.importFile("https://s3.amazonaws.com/erin-data/higgs/higgs_train_10k.csv")
+test <- h2o.importFile("https://s3.amazonaws.com/erin-data/higgs/higgs_test_5k.csv")
+
+# Identify predictors and response
+y <- "response"
+x <- setdiff(names(train), y)
+
+# For binary classification, response should be a factor
+train[, y] <- as.factor(train[, y])
+test[, y] <- as.factor(test[, y])
+
+# Run AutoML for 20 base models (limited to 1 hour max runtime by default)
+aml <- h2o.automl(x = x, y = y,
+                  training_frame = train,
+                  max_models = 20,
+                  seed = 1)
+
+# View the AutoML Leaderboard
+lb <- aml@leaderboard
+print(lb, n = nrow(lb))  # Print all rows instead of default (6 rows)
